0944184244fd045e0259d9b3b0dbefefa7c9d216
fumio <fumio@fumio-virtual-machine>
2015-02-16 17:32:26 +0900

    figure1を少し触った

--- chapter2/figure1.py
+++ chapter2/figure1.py
@@ -14,15 +14,16 @@ features = data['data']
 feature_names = data['feature_names']
 target = data['target']
 
-
 pairs = [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)]
 for i, (p0, p1) in enumerate(pairs):
     plt.subplot(2, 3, i + 1)
     for t, marker, c in zip(range(3), ">ox", "rgb"):
         plt.scatter(features[target == t, p0], features[
                     target == t, p1], marker=marker, c=c)
+        
+    #plt.legend(['%s' % data['target_names'][0], '%s' % data['target_names'][1], '%s' % data['target_names'][2]], loc = 'upper left')
     plt.xlabel(feature_names[p0])
     plt.ylabel(feature_names[p1])
     plt.xticks([])
     plt.yticks([])
-plt.savefig('../1400_02_01.png')
+plt.show()
